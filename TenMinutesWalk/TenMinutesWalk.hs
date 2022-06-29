60afb4892df0a508d8b56f2c6d4225847ec5e9ef
PaperBackPear3 <fabioangeloni2001@gmail.com>
2022-06-29 22:52:56 +0200

    added comment

---
+++ TenMinutesWalk/TenMinutesWalk.hs
@@ -0,0 +1,6 @@
+module Codewars.Kata.TenMinuteWalk where
+
+isValidWalk :: [Char] -> Bool
+isValidWalk walk = 
+
+-- assign +1 to n -1 to s and +1 to w, -1 to e if the final sum is 0 then we're on the same place as we started
